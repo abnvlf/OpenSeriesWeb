b4fc388652f0e3201e1d245bc8a30eace6ca16fb
envilion s. <pow.zone@outlook.com>
2024-02-01 19:27:28 +0700

    feat: playground init

---
+++ app/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { promisify } from "util";
+import { exec } from "child_process";
+
+const execPromise = promisify(exec);
+
+const getPythonCommand = async () => {
+    const { stdout: python3Stdout } = await execPromise("python3 --version").catch(({ stdout }) => ({ stdout }));
+    const python3 = python3Stdout ? "python3" : "python";
+    const { stdout: pythonStdout } = await execPromise("python --version").catch(({ stdout }) => ({ stdout }));
+    const python = pythonStdout ? "python" : python3;
+
+    return python;
+};
+
+export async function compilePy(prevState: any, formData: FormData) {
+    const code = formData.get("code");
+    const python = await getPythonCommand();
+    const pythonCmd = `${python} -c "${code}"`;
+    const runResult = await execPromise(pythonCmd).catch((val) => ({ stdout: val.stderr, stderr: val.stderr }));
+
+    return {
+        result: runResult.stderr ? runResult.stderr : runResult.stdout,
+        message: "Python code executed successfully",
+        success: runResult.stderr ? false : true
+    };
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,130 @@
-import Image from "next/image";
+"use client";
+
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { useCallback, useRef, useState } from "react";
+import { python } from "@codemirror/lang-python";
+import { tokyoNightStorm } from "@uiw/codemirror-theme-tokyo-night-storm";
+import { compilePy } from "./actions";
+import { useFormState, useFormStatus } from "react-dom";
+
+const initialState = {
+    message: "",
+    result: "",
+    success: false
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [value, setValue] = useState(`import OpenSeries.matematika as matematika
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+radian = 1
+print(matematika.radian_ke_derajat(radian))
+# 57,296
+`);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    const textarea = useRef<any>();
+
+    const onChange = useCallback((val: string) => {
+        setValue(val);
+        textarea.current!.value = val;
+    }, []);
+
+    const [state, formAction] = useFormState(compilePy, initialState);
+
+    return (
+        <main className="flex max-h-dvh min-h-dvh flex-col overflow-hidden">
+            <header className="flex items-center justify-between border-b border-b-gray-600 bg-[#24283B] px-6 py-2">
+                <div className="flex items-center gap-4">
+                    <h1 className="text-xl font-bold text-white">
+                        <span className="bg-gradient-to-br from-indigo-600 to-rose-400 bg-clip-text font-bold text-transparent">
+                            OpenSeries
+                        </span>{" "}
+                        Playground
+                    </h1>
+                    {state?.message && (
+                        <span
+                            className={`rounded-full px-4 py-1 text-xs font-medium text-white ${state?.success ? "bg-green-600" : "bg-red-600"}`}
+                        >
+                            {state?.message}
+                        </span>
+                    )}
+                </div>
+                <div className="flex items-center gap-2">
+                    <button
+                        className="rounded-md bg-fuchsia-600 px-8 py-3 font-medium text-white"
+                        onClick={() => alert("blm bisa coy")}
+                    >
+                        Share
+                    </button>
+                    <form action={formAction}>
+                        <textarea
+                            name="code"
+                            id="code"
+                            className="sr-only"
+                            ref={textarea}
+                            defaultValue={value}
+                        ></textarea>
+                        <Submit />
+                    </form>
+                </div>
+            </header>
+            <form className="grid grow grid-cols-2 divide-x divide-gray-600 overflow-y-auto">
+                <ReactCodeMirror
+                    value={value}
+                    theme={tokyoNightStorm}
+                    className="h-full overflow-auto bg-[#24283B]"
+                    extensions={[python()]}
+                    onChange={onChange}
+                />
+                <div className="h-full overflow-auto bg-[#24283B] p-8">
+                    <h2 className="mb-4 text-xl font-bold text-white">Output: </h2>
+                    <pre className="w-full overflow-auto text-white">
+                        <code>{state?.result}</code>
+                    </pre>
+                </div>
+            </form>
+        </main>
+    );
+}
+
+function Submit() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            disabled={pending}
+            className={`grid h-12 w-12 place-items-center rounded-md bg-indigo-600 ${pending ? "cursor-not-allowed opacity-50" : ""}`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            {pending ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6 animate-spin stroke-white"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                </svg>
+            ) : (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6 stroke-white"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                    />
+                </svg>
+            )}
+        </button>
+    );
 }
