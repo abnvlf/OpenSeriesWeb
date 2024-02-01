fdeba732a6af8a47aaa9713174e1acbc0b58da13
envilion s. <pow.zone@outlook.com>
2024-02-01 19:58:54 +0700

    feat: server action test 2

--- app/actions.ts
+++ app/actions.ts
@@ -1,21 +1,21 @@
 "use server";
 
-import { promisify } from "util";
-import { exec } from "child_process";
+// import { promisify } from "util";
+// import { exec } from "child_process";
 
-const execPromise = promisify(exec);
+// const execPromise = promisify(exec);
 
-const getPythonCommand = async () => {
-    const { stdout: python3Stdout } = await execPromise("python3 --version").catch(({ stdout }) => ({ stdout }));
-    const python3 = python3Stdout ? "python3" : "python";
-    const { stdout: pythonStdout } = await execPromise("python --version").catch(({ stdout }) => ({ stdout }));
-    const python = pythonStdout ? "python" : python3;
+// const getPythonCommand = async () => {
+//     const { stdout: python3Stdout } = await execPromise("python3 --version").catch(({ stdout }) => ({ stdout }));
+//     const python3 = python3Stdout ? "python3" : "python";
+//     const { stdout: pythonStdout } = await execPromise("python --version").catch(({ stdout }) => ({ stdout }));
+//     const python = pythonStdout ? "python" : python3;
 
-    return python;
-};
+//     return python;
+// };
 
 export async function compilePy(prevState: any, formData: FormData) {
-    const code = formData.get("code");
+    // const code = formData.get("code");
     // const python = await getPythonCommand();
     // const pythonCmd = `${python} -c "${code}"`;
     // const runResult = await execPromise(pythonCmd).catch((val) => ({ stdout: val.stderr, stderr: val.stderr }));
