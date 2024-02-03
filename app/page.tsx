a67f9676389b329560597d5a4be1337685e69a4e
envilion s. <pow.zone@outlook.com>
2024-02-04 00:56:29 +0700

    feat: all fallback if run comsume more than 5 sec

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
 import OpenSeries.matematika as matematika
 import OpenSeries.fisika as fisika
-from OpenSeries import statistika as statistika
+import OpenSeries.statistika as statistika
 import numpy as np
 
 radian = 1
@@ -57,101 +57,117 @@ print('standar_deviasi', hasil)
 
     const [isLoading, setIsLoading] = useState(false);
     const [result, setResult] = useState<{ message: string; result: string; success: boolean }>();
+    const [isStartupApi, setIsStartupApi] = useState(false);
 
     const onRun = async () => {
         setIsLoading(true);
 
-        const response = await fetch(String(process.env.NEXT_PUBLIC_RUN_API), {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ code })
-        });
+        const timeoutId = setTimeout(() => {
+            setIsStartupApi(true);
+        }, 5000);
 
-        const data = await response.json();
-        setResult(data);
-        setIsLoading(false);
+        try {
+            const response = await fetch(String(process.env.NEXT_PUBLIC_RUN_API), {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ code })
+            });
+
+            const data = await response.json();
+            setResult(data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            clearTimeout(timeoutId);
+            setIsLoading(false);
+        }
     };
 
     return (
-        <main className="flex max-h-dvh min-h-dvh flex-col overflow-hidden">
-            <header className="flex items-center justify-between border-b px-6 py-2">
-                <div className="flex items-center gap-4">
-                    <h1 className="text-xl font-bold text-zinc-800">
-                        <span className="bg-gradient-to-br from-indigo-600 to-rose-400 bg-clip-text font-bold text-transparent">
-                            OpenSeries
-                        </span>{" "}
-                        Playground
-                    </h1>
-                    {result?.message && (
-                        <span
-                            className={`rounded-full px-4 py-1 text-xs font-medium text-white ${result?.success ? "bg-green-600" : "bg-red-600"}`}
-                        >
-                            {result?.message}
-                        </span>
-                    )}
-                </div>
-                <div className="flex items-center gap-2">
+        <>
+            <div
+                className={`fixed inset-0 z-50 grid place-items-center ${isStartupApi ? "pointer-events-auto" : "pointer-events-none"}`}
+            >
+                <div
+                    onClick={() => setIsStartupApi(false)}
+                    className={`absolute inset-0 bg-zinc-900/40 transition-all duration-300 ${isStartupApi ? "opacity-100" : "opacity-0"}`}
+                ></div>
+                <div
+                    className={`w-full max-w-md space-y-6 rounded-lg bg-white p-12 text-center transition-all duration-300 ${isStartupApi ? "scale-100 opacity-100" : "scale-75 opacity-0"}`}
+                >
+                    <span className="text-9xl">🥶</span>
+                    <div className="space-y-1">
+                        <h1 className="text-2xl font-bold">Wow! Kamu Adalah Superhero!</h1>
+                        <p className="text-zinc-600">
+                            OpenSeries Playground sedang tertidur sebelumnya! Kamu saat ini sedang membangunkannya!
+                            Tolong tunggu beberapa saat sampai dia bangun ya! 😁
+                        </p>
+                    </div>
                     <button
-                        className="rounded-md bg-fuchsia-600 px-8 py-3 font-medium text-white"
-                        onClick={() => alert("blm bisa coy")}
+                        onClick={() => setIsStartupApi(false)}
+                        className={`w-full rounded-md bg-indigo-600 px-6 py-2.5 font-medium text-indigo-100`}
                     >
-                        Share
+                        Okay
                     </button>
-                    <button
-                        disabled={isLoading}
-                        className={`grid h-12 w-12 place-items-center rounded-md bg-indigo-600`}
-                        onClick={onRun}
-                    >
-                        {isLoading ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="h-6 w-6 animate-spin stroke-white"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                                />
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="h-6 w-6 stroke-white"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
-                                />
-                            </svg>
-                        )}
-                    </button>
-                </div>
-            </header>
-            <div className="grid grow grid-cols-2 divide-x overflow-y-auto">
-                <Editor
-                    className="h-full"
-                    defaultLanguage="python"
-                    defaultValue={code}
-                    onChange={(val) => setCode(val!)}
-                />
-                <div className="h-full overflow-auto p-8">
-                    <h2 className="mb-4 text-xl font-bold text-zinc-800">Output: </h2>
-                    <pre className="w-full overflow-auto text-zinc-800">
-                        <code>{result?.result}</code>
-                    </pre>
+                    <span className="inline-block text-sm text-zinc-400">
+                        Hanya orang tertentu yang bisa mendapatkan pesan ini
+                    </span>
                 </div>
             </div>
-        </main>
+            <main className="flex max-h-dvh min-h-dvh flex-col overflow-hidden">
+                <header className="flex items-center justify-between border-b px-6 py-2">
+                    <div className="flex items-center gap-4">
+                        <h1 className="text-xl font-bold text-zinc-800">
+                            <span className="bg-gradient-to-br from-indigo-600 to-rose-400 bg-clip-text font-bold text-transparent">
+                                OpenSeries
+                            </span>{" "}
+                            Playground
+                        </h1>
+                        {result?.message && (
+                            <span
+                                className={`rounded-full px-4 py-1 text-xs font-medium text-white ${result?.success ? "bg-green-600" : "bg-red-600"}`}
+                            >
+                                {result?.message}
+                            </span>
+                        )}
+                    </div>
+                    <div className="flex items-center gap-1">
+                        <button
+                            className={`rounded-md border-2 border-indigo-600 px-6 py-2.5 font-medium text-indigo-600 hover:bg-indigo-100`}
+                            onClick={() => alert("blm bisa coy")}
+                        >
+                            Share
+                        </button>
+                        <button
+                            disabled={isLoading}
+                            className={`grid h-12 w-12 place-items-center rounded-md bg-indigo-600`}
+                            onClick={onRun}
+                        >
+                            {isLoading ? (
+                                <span className="icon-[ri--loader-3-line] h-6 w-6 animate-spin text-white"></span>
+                            ) : (
+                                <span className="icon-[iconamoon--player-play] h-6 w-6 text-white"></span>
+                            )}
+                        </button>
+                    </div>
+                </header>
+                <div className="grid grow grid-cols-2 divide-x overflow-y-auto">
+                    <Editor
+                        className="h-full"
+                        defaultLanguage="python"
+                        defaultValue={code}
+                        onChange={(val) => setCode(val!)}
+                    />
+                    <div className="h-full overflow-auto p-8">
+                        <h2 className="mb-4 text-xl font-bold text-zinc-800">Output: </h2>
+                        <pre className="w-full overflow-auto text-zinc-800">
+                            <code>{result?.result}</code>
+                        </pre>
+                    </div>
+                </div>
+            </main>
+        </>
     );
 }
